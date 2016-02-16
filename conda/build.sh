673bd40ac57ddb8a83fa641e831cd6203f10dbfc
jay <jlaura@usgs.gov>
2016-02-16 14:49:18 -0700

    Trying to build 1.11.3

--- conda/build.sh
+++ conda/build.sh
@@ -1,33 +1,39 @@
 #!/bin/bash
 
-# http://www.michael-joost.de/gdal_install.html
-unset CC CPP CXX
-
-if [ `uname` == Darwin ]; then
-    export LDFLAGS="$LDFLAGS -headerpad_max_install_names"
+if [ $(uname) == Darwin ]; then
+    PGFLAG=""
+    export LDFLAGS="-headerpad_max_install_names"
+else
+    PGFLAG="--with-pg=$PREFIX/bin/pg_config"
 fi
 
-bash configure \
-    --without-libtool \
-    --with-python \
-    --with-hdf4=$PREFIX \
-    --with-hdf5=$PREFIX \
-    --with-xerces=$PREFIX \
-    --with-netcdf=$PREFIX \
-    --with-geos=$PREFIX/bin/geos-config \
-    --with-kea=$PREFIX/bin/kea-config \
-    --prefix=$PREFIX
+CPPFLAGS="-I$PREFIX/include" LDFLAGS="-L$PREFIX/lib" \
+./configure --prefix=$PREFIX \
+            --with-geos=$PREFIX/bin/geos-config \
+            --with-static-proj4=$PREFIX \
+            --with-hdf5=$PREFIX \
+            --with-hdf4=$PREFIX \
+            --with-xerces=$PREFIX \
+            --with-netcdf=$PREFIX \
+            --with-openjpeg=$PREFIX \
+            --with-python \
+            --disable-rpath \
+            --without-pam \
+            $PGFLAG
+
 make
 make install
 
-# Make sure GDAL_DATA and set and still present in the package
-# https://github.com/conda/conda-recipes/pull/267
-ACTIVATE_DIR=$PREFIX/etc/conda/activate.d
-DEACTIVATE_DIR=$PREFIX/etc/conda/deactivate.d
-mkdir -p $ACTIVATE_DIR
-mkdir -p $DEACTIVATE_DIR
+# Copy data files.
+mkdir -p $PREFIX/share/gdal/
+cp data/*csv $PREFIX/share/gdal/
+cp data/*wkt $PREFIX/share/gdal/
 
-cp $RECIPE_DIR/scripts/activate.sh $ACTIVATE_DIR/gdal-activate.sh
-cp $RECIPE_DIR/scripts/deactivate.sh $DEACTIVATE_DIR/gdal-deactivate.sh
-
-
+if [ $(uname) == Darwin ]; then
+    # Copy TIFF and GEOTIFF Headers so can build against gdal internal geotiff/tiff libraries.
+    mkdir -p $PREFIX/include/gdal/frmts/gtiff/libgeotiff
+    cp frmts/gtiff/libgeotiff/*.h $PREFIX/include/gdal/frmts/gtiff/libgeotiff
+    cp frmts/gtiff/libgeotiff/*.inc $PREFIX/include/gdal/frmts/gtiff/libgeotiff
+    mkdir -p $PREFIX/include/gdal/frmts/gtiff/libtiff
+    cp frmts/gtiff/libtiff/*.h $PREFIX/include/gdal/frmts/gtiff/libtiff
+fi
